77f3080141deb467f9196005a8da9003bd8168e0
marusiakulyk <mary.kulyk@gmail.com>
2019-11-27 18:19:59 +0200

    initial commit

---
+++ build/CY8CKIT-062-BLE/Debug/CypressLogo.d
@@ -0,0 +1,20 @@
+/home/marusia/mtw/emWin_E_Ink_FreeRTOS/build/CY8CKIT-062-BLE/Debug/CypressLogo.o: \
+ CypressLogo.c libs/emwin/GUI/Include/GUI.h \
+ libs/emwin/GUI/Include/GUI_ConfDefaults.h \
+ libs/emwin/GUI/COMPONENT_SOFTFP/COMPONENT_EMWIN_OSNTS/GUIConf.h \
+ libs/emwin/GUI/Include/GUI_Type.h libs/emwin/GUI/Include/LCD.h \
+ libs/emwin/GUI/Include/Global.h libs/emwin/GUI/Include/GUI_Version.h
+
+libs/emwin/GUI/Include/GUI.h:
+
+libs/emwin/GUI/Include/GUI_ConfDefaults.h:
+
+libs/emwin/GUI/COMPONENT_SOFTFP/COMPONENT_EMWIN_OSNTS/GUIConf.h:
+
+libs/emwin/GUI/Include/GUI_Type.h:
+
+libs/emwin/GUI/Include/LCD.h:
+
+libs/emwin/GUI/Include/Global.h:
+
+libs/emwin/GUI/Include/GUI_Version.h:
---
+++ build/CY8CKIT-062-BLE/Debug/emwin_config/GUIConf.d
@@ -0,0 +1,20 @@
+/home/marusia/mtw/emWin_E_Ink_FreeRTOS/build/CY8CKIT-062-BLE/Debug/emwin_config/GUIConf.o: \
+ emwin_config/GUIConf.c libs/emwin/GUI/Include/GUI.h \
+ libs/emwin/GUI/Include/GUI_ConfDefaults.h \
+ libs/emwin/GUI/COMPONENT_SOFTFP/COMPONENT_EMWIN_OSNTS/GUIConf.h \
+ libs/emwin/GUI/Include/GUI_Type.h libs/emwin/GUI/Include/LCD.h \
+ libs/emwin/GUI/Include/Global.h libs/emwin/GUI/Include/GUI_Version.h
+
+libs/emwin/GUI/Include/GUI.h:
+
+libs/emwin/GUI/Include/GUI_ConfDefaults.h:
+
+libs/emwin/GUI/COMPONENT_SOFTFP/COMPONENT_EMWIN_OSNTS/GUIConf.h:
+
+libs/emwin/GUI/Include/GUI_Type.h:
+
+libs/emwin/GUI/Include/LCD.h:
+
+libs/emwin/GUI/Include/Global.h:
+
+libs/emwin/GUI/Include/GUI_Version.h:
